3f233c79b25431dc8be915b89233ee0c30d9f3ca
Angelinagustin <160854942+Angelinagustin@users.noreply.github.com>
2024-11-13 18:39:36 +0700

    add : perbaikan
    .

--- flutter Angelin/flutter_application_1/lib/screens/detail_screen.dart
+++ flutter Angelin/flutter_application_1/lib/screens/detail_screen.dart
@@ -12,30 +12,53 @@ class DetailScreen extends StatelessWidget {
         children: [
           // Detail header
           // image utama
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                candi.imageAsset,
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
-              ),
-            ),
-          )
-          // tombol back
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.deepPurple[100]?.withOpacity(0.8),
-                shape: BoxShape.circle,
-              ),
-              child: IconButton(
-                onPressed: () {},
-                icon:  const Icon(Icons.arrow_back)
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
                 ),
+              ),
+              // tombol back
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+                ),
+              ),
+            ],
+          ),
+          // Detail Info
+          // info atas
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                // info atas
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(candi.name),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite_border))
+                  ],
+                )
+                // info tengah
+                // imfo bawah
+              ],
             ),
           )
         ],
